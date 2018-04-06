394d2d27286b546e0f5aa6e1239c8473688129a5
Atkins, Leona <leona.atkins@metoffice.gov.uk>
2018-04-06 11:00:36 +0100

    Replace code with something a bit more stable and add custom mocked data, again this is just for a demo, not production!

--- server.js
+++ server.js
@@ -1,32 +1,70 @@
-var app = require('http').createServer(handler),
-    fs = require('fs')
+var http = require('http');
+var fs = require('fs');
 
-app.listen(8080);
+http.createServer(function(req, res) {
+  debugHeaders(req);
 
-function handler(req, res) {
-  if (req.url == '/index.html' || req.url == '/') {
-    fs.readFile(__dirname + '/index.html',
-    function (err, data) {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error loading index.html: ' + err);
-      }
-      res.writeHead(200);
-      res.end(data);
-    });
-  }
-  else if (req.url == '/stream') {
-    res.writeHead(200, {
-      'Content-type': 'text/event-stream',
-      'Cache-control': 'no-cache',
-      'Connection': 'keep-alive'
-    });
-    setInterval(function() {
-      res.write('data: hello!\n\n');
-    }, 2000);
-  }
-  else {
-    res.writeHead(404);
+  if (req.headers.accept && req.headers.accept === 'text/event-stream') {
+    if (req.url === '/stream') {
+      sendSSE(req, res);
+    } else if (req.url === '/') {
+      sendSSE(req, res);
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  } else {
     res.end();
   }
+}).listen(8080);
+
+function sendSSE(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    // Website you wish to allow to connect
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+
+    // Request methods you wish to allow
+    'Access-Control-Allow-Methods':'GET',
+
+    // Request headers you wish to allow
+    'Access-Control-Allow-Headers':'X-Requested-With,content-type',
+    'Access-Control-Allow-Credentials': true
+
+  });
+
+  // Sends a SSE every 5 seconds on a single connection.
+  setInterval(function() {
+    constructSSE(res);
+  }, 10*1000);
+
+  constructSSE(res);
+}
+
+function constructSSE(res) {
+  var randomNumber = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+  var index = randomNumber-1;
+  var satArray = [
+    [{id:1,state:'OK'},{id:2,state:'OK'},{id:3,state:'OK'},{id:4,state:'OK'}],
+    [{id:1,state:'OK'},{id:2,state:'OK'},{id:3,state:'OK'},{id:4,state:'ERROR'}],
+    [{id:1,state:'OK'},{id:2,state:'OK'},{id:3,state:'ERROR'},{id:4,state:'OK'}],
+    [{id:1,state:'OK'},{id:2,state:'ERROR'},{id:3,state:'OK'},{id:4,state:'OK'}],
+    [{id:1,state:'ERROR'},{id:2,state:'OK'},{id:3,state:'OK'},{id:4,state:'OK'}],
+  ];
+  var data = JSON.stringify(satArray[index]);
+  var id = Math.floor(Math.random() * 99999999999);
+  console.log("id: "+id);
+  console.log("data: "+data);
+  res.write('id: ' + id + '\n');
+  res.write("data: " + data+'\n\n');
+}
+
+function debugHeaders(req) {
+  console.log('URL: ' + req.url);
+  for (var key in req.headers) {
+    console.log(key + ': ' + req.headers[key]);
+  }
+  console.log('\n\n');
 }
